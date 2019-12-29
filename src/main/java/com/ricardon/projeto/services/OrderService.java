ab9da55e315157d68dbe94d770674caf29a352f7
ricardon91 <ricardo_l.h.p@hotmail.com>
2019-12-29 15:45:45 -0300

    Order, Instant, ISO 8601

---
+++ src/main/java/com/ricardon/projeto/services/OrderService.java
@@ -0,0 +1,27 @@
+package com.ricardon.projeto.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ricardon.projeto.entities.Order;
+import com.ricardon.projeto.repositories.OrderRepository;
+
+@Service
+public class OrderService {
+	
+	@Autowired
+	private OrderRepository repository;
+	
+	public List<Order> findAll(){
+		return repository.findAll();
+	}
+	
+	public Order findById(Long id){
+		Optional<Order> obj = repository.findById(id);
+		return obj.get();
+	}
+
+}
