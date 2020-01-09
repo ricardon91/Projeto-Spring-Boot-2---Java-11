9f4cb44ae82ca36108acb0532d81eb3c38063a04
ricardon91 <ricardo_l.h.p@hotmail.com>
2020-01-09 16:15:52 -0300

    Payment, one-to-one association

--- src/main/java/com/ricardon/projeto/config/TestConfig.java
+++ src/main/java/com/ricardon/projeto/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.ricardon.projeto.entities.Category;
 import com.ricardon.projeto.entities.Order;
 import com.ricardon.projeto.entities.OrderItem;
+import com.ricardon.projeto.entities.Payment;
 import com.ricardon.projeto.entities.Product;
 import com.ricardon.projeto.entities.User;
 import com.ricardon.projeto.entities.enums.OrderStatus;
@@ -78,6 +79,11 @@ public class TestConfig implements CommandLineRunner{
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 	
 	
