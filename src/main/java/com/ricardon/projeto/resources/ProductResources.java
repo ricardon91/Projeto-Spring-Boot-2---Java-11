01194b7e169d51ffd7950f17822b505df25cbf61
ricardon91 <ricardo_l.h.p@hotmail.com>
2020-01-09 13:15:36 -0300

    Product resourse and service

---
+++ src/main/java/com/ricardon/projeto/resources/ProductResources.java
@@ -0,0 +1,34 @@
+package com.ricardon.projeto.resources;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ricardon.projeto.entities.Product;
+import com.ricardon.projeto.services.ProductService;
+
+@RestController
+@RequestMapping(value = "/producties")
+public class ProductResources {
+	
+	@Autowired
+	private ProductService service;
+	
+	@GetMapping
+	public ResponseEntity<List<Product>> findAll(){		
+		List<Product> list = service.findAll();		
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Product> findById(@PathVariable Long id){
+		Product obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
+
+}
